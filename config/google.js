0a66aec37f2b94b017aeabc2ea22c58da5b738e3
Vlad <1904640+axmachina@users.noreply.github.com>
2024-04-07 22:21:35 -0700

    config/google

--- config/google.js
+++ config/google.js
@@ -2,18 +2,25 @@ function croak(msg) {
   throw new Error(msg);
 }
 
-const config = {
-  g_client_id: process.env.g_client_id || croak("Missing g_client_id"),
-  g_client_secret: process.env.g_client_secret || croak("Missing g_client_secret"),
-  g_redirect_uri: process.env.g_redirect_uri || "http://localhost:8000/account/oauth/google",
-}
+var client_id = process.env.g_client_id || croak("Missing g_client_id"),
+  client_secret = process.env.g_client_secret || croak("Missing g_client_secret"),
+  redirect_uri =
+    process.env.g_redirect_uri || "http://localhost:8000/account/oauth/google",
+  auth_url =
+    "https://accounts.google.com/o/oauth2/v2/auth?response_type=code&redirect_uri=" +
+    redirect_uri +
+    "&scope=profile%20email&client_id=" +
+    client_id;
 
-config.auth_url = "https://accounts.google.com/o/oauth2/v2/auth?response_type=code&redirect_uri=" + config.g_redirect_uri + "&scope=profile%20email&client_id=" + config.g_client_id;
+    // print out config props and values for debugging
+    console.log("client_id: " + client_id);
+    console.log("client_secret: " + client_secret);
+    console.log("redirect_uri: " + redirect_uri);
+    console.log("auth_url: " + auth_url);
 
-// print out config props and values for debugging
-console.log("g_client_id: " + config.g_client_id);
-console.log("g_client_secret: " + config.g_client_secret);
-console.log("g_redirect_uri: " + config.g_redirect_uri);
-console.log("auth_url: " + config.auth_url);
-
-module.exports = config;
+module.exports = {
+  client_id: client_id,
+  client_secret: client_secret,
+  auth_url: auth_url,
+  redirect_uri: redirect_uri
+};
